e4ccee51eb3f6c05e99b2c88e76c4fef7257b6c5
Billy Atim <atimb287@gmail.com>
2023-05-11 15:20:28 +0100

    ready for mac

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Image from 'next/image'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
-import Banner from '../components/banner'
-export default function Home() {
+import Banner from "../components/banner";
+import Card from "../components/card";
 
-  const handleOnBannerBtnClick = () => {
-    
-    console.log("Yoo");
+import coffeeStoresData from "../data/coffee-stores.json";
+
+export async function getStaticProps(context) {
+  
+  return {
+      props: {
+        coffeeStores:coffeeStoresData
+      },
   }
+}
+
+export default function Home(props) {
+  console.log('props', props);
+  const handleOnBannerBtnClick = () => {
+    console.log("Yoo");
+  };
 
   return (
     <div className={styles.container}>
@@ -19,15 +31,33 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        
-         <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerBtnClick} />
+        <Banner
+          buttonText="View stores nearby"
+          handleOnClick={handleOnBannerBtnClick}
+        />
         <div className={styles.heroImage}>
-
-       
-           <Image src="/static/coffee-hero-img.png" width={700} height={400}/>
-            </div> </main>
-
-     
+          <Image src="/static/coffee-hero-img.png" width={700} height={400} />
+        </div>
+        {props.coffeeStores.length > 0 && 
+          <>
+          <h2 className={styles.heading2}> Toronto stores</h2>
+        <div className={styles.cardLayout}>
+          {props.coffeeStores.map((coffeeStore) => {
+            return (
+              <Card
+              id={coffeeStore.id}
+                name={coffeeStore.name}
+                imgUrl={coffeeStore.imgUrl}
+                href={`/coffee-store/${coffeeStore.id}`}
+                className={styles.card}
+              />
+            );
+          })}
+        </div>
+        </>
+        }
+        
+      </main>
     </div>
-  )
+  );
 }
